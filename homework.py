1b65ac13f265ba06661507f6a2c12e0bd01e0f21
Elena <elenakolesnikova141@gmail.com>
2024-10-07 18:16:58 +0500

    Атотест2 на правильное переключение темной темы

--- homework.py
+++ homework.py
@@ -16,3 +16,23 @@ def test_dark_theme_by_time():
 
     assert is_dark_theme is True
 
+def test_dark_theme_by_time_and_user_choice():
+    """
+    Протестируйте правильность переключения темной темы на сайте
+    в зависимости от времени и выбора пользователя
+    dark_theme_enabled_by_user = True - Темная тема включена
+    dark_theme_enabled_by_user = False - Темная тема выключена
+    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
+    """
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
+    # TODO переключите темную тему в зависимости от времени суток,
+    #  но учтите что темная тема может быть включена вручную
+
+    is_dark_theme = dark_theme_enabled_by_user
+    if dark_theme_enabled_by_user is None:
+        is_dark_theme = current_time >= time(hour=22) or current_time < time(hour=6)
+
+    assert is_dark_theme is True
+
+
